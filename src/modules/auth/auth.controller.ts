2ef33d52425301e27648e7c1286331b77a586b6f
Hurmaan <contactme.hurmaan@gmail.com>
2023-12-23 15:46:43 +0530

    feat: define dummy create routes for signin, signup  +add validators as DTOs

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+// validation
+class SignupDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  apikey: string;
+}
+
+class SigninDto {
+  username: string;
+  password: string;
+}
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('signup')
+  create(@Body() signupDto: SignupDto): string {
+    console.log(signupDto);
+    return 'Data received successfully!';
+  }
+
+  @Post('signin')
+  login(@Body() signinDto: SigninDto): string {
+    console.log(signinDto);
+    return 'Data received successfully!';
+  }
+}
