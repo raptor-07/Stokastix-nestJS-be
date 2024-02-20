e75da585c2c2d1d13136d348d7a2cae3675b59f8
Hurmaan <contactme.hurmaan@gmail.com>
2024-02-21 01:15:27 +0530

    refractor: side effects trade module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,8 @@ async function bootstrap() {
   // app.useGlobalPipes(new ValidationPipe());
 
   //enable cors for localhost:3000
+  app.useLogger(new Logger('global'));
+
   app.enableCors();
 
   await app.listen(4000);
