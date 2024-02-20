7afba6d8117a99e31bb670a555285395e5903d98
Hurmaan <contactme.hurmaan@gmail.com>
2024-02-20 20:52:46 +0530

    feat: create util types for trade module

---
+++ src/modules/trade/dto/trade.configDto.ts
@@ -0,0 +1,38 @@
+export class ConfigDto {
+    botMode: string;
+    asset: string;
+    autoSwitch: boolean;
+    trendTf: string;
+    botSide: string;
+    indicator: string;
+    slTf: string;
+    entryTf: string;
+    exitTf: string;
+    exitMode: string;
+    alloc: number;
+    numOfTrades: number;
+    emaLength: number;
+    targetPrice: number;
+    maxLeverage: number;
+    startTime: string;
+    endTime: string;
+}
+
+// config.botMode,
+// config.asset,
+// config.autoSwitch,
+// config.trendTf,
+// config.botSide,
+// config.indicator,
+// config.slTf,
+// config.entryTf,
+// config.exitTf,
+// config.exitMode,
+// config.alloc,
+// config.numOfTrades,
+// config.emaLength,
+// config.targetPrice,
+// config.maxLeverage,
+// "start",
+// config.startTime,
+// config.endTime
---
+++ src/modules/trade/dto/trade.fetchIntervalStateDto.ts
@@ -0,0 +1,9 @@
+export class FetchIntervalState{
+    _3m: Boolean;
+    _15m: Boolean;   
+    _30m: Boolean;
+    _1h: Boolean;
+    _4h: Boolean;
+    _8h: Boolean;
+    _1d: Boolean;
+}
